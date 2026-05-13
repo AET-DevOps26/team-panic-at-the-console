49d0dbc8ab90c88db2a3887fb91ea132f43751e3
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2026-05-14 00:31:28 +0200

    use generated server stubs

--- services/incident-service/src/main/java/com/panicattheconsole/incidentservice/genai/GenaiRegenController.java
+++ services/incident-service/src/main/java/com/panicattheconsole/incidentservice/genai/GenaiRegenController.java
@@ -1,45 +1,45 @@
 package com.panicattheconsole.incidentservice.genai;
 
+import org.openapitools.api.GenaiApi;
+import org.openapitools.model.RegenAccepted;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/incidents/{incidentId}/genai")
-class GenaiRegenController {
+class GenaiRegenController implements GenaiApi {
 
     private static final Logger log = LoggerFactory.getLogger(GenaiRegenController.class);
 
-    record RegenAccepted(boolean accepted, String task) {}
-
-    @PostMapping("/summary")
-    ResponseEntity<RegenAccepted> regenerateSummary(@PathVariable("incidentId") UUID incidentId) {
+    @Override
+    public ResponseEntity<RegenAccepted> regenerateSummary(UUID incidentId) {
         log.info("TODO: publish incident.regen.requested [task=SUMMARY, incidentId={}]", incidentId);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new RegenAccepted(true, "SUMMARY"));
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(new RegenAccepted(true, RegenAccepted.TaskEnum.SUMMARY));
     }
 
-    @PostMapping("/severity")
-    ResponseEntity<RegenAccepted> regenerateSeverity(@PathVariable("incidentId") UUID incidentId) {
+    @Override
+    public ResponseEntity<RegenAccepted> regenerateSeverity(UUID incidentId) {
         log.info("TODO: publish incident.regen.requested [task=SEVERITY_SUGGESTION, incidentId={}]", incidentId);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new RegenAccepted(true, "SEVERITY_SUGGESTION"));
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(new RegenAccepted(true, RegenAccepted.TaskEnum.SEVERITY_SUGGESTION));
     }
 
-    @PostMapping("/solutions")
-    ResponseEntity<RegenAccepted> regenerateSolutions(@PathVariable("incidentId") UUID incidentId) {
+    @Override
+    public ResponseEntity<RegenAccepted> regenerateSolutions(UUID incidentId) {
         log.info("TODO: publish incident.regen.requested [task=SOLUTION_SUGGESTIONS, incidentId={}]", incidentId);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new RegenAccepted(true, "SOLUTION_SUGGESTIONS"));
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(new RegenAccepted(true, RegenAccepted.TaskEnum.SOLUTION_SUGGESTIONS));
     }
 
-    @PostMapping("/postmortem")
-    ResponseEntity<RegenAccepted> regeneratePostmortem(@PathVariable("incidentId") UUID incidentId) {
+    @Override
+    public ResponseEntity<RegenAccepted> regeneratePostmortem(UUID incidentId) {
         log.info("TODO: publish incident.regen.requested [task=POSTMORTEM, incidentId={}]", incidentId);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new RegenAccepted(true, "POSTMORTEM"));
+        return ResponseEntity.status(HttpStatus.ACCEPTED)
+                .body(new RegenAccepted(true, RegenAccepted.TaskEnum.POSTMORTEM));
     }
 }
